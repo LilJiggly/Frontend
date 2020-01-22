bec6b2bcc68426e0c7ef05a19b6806e221e971df
LilJiggly <58260324+LilJiggly@users.noreply.github.com>
2020-01-22 16:38:37 +0100

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,18 @@
 console.log("ik ben gelinkt");
+
+// h1 word grooter
+var heen = document.querySelector("h1");
+
+function groot() {
+  heen.classList.toggle("groot");
+}
+
+heen.addEventListener("click", groot);
+
+console.log(heen);
+
+/* button veranderd van tekst */
+submit.addEventListener("click", function() {
+  document.getElementById("submit").value = "Bedankt voor het invullen!";
+  console.log("Ik ben geklikt");
+});
